52248cea05335225e63c7fa606e599ec8a62b5cf
surendra246 <surendrasonkar@gmail.com>
2025-10-25 13:40:39 +0530

    Initial project setup

---
+++ src/main/java/com/banking/paymentrequest/PaymentrequestApplication.java
@@ -0,0 +1,13 @@
+package com.banking.paymentrequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentrequestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentrequestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banking/paymentrequest/PaymentrequestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.paymentrequest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentrequestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
